5a0a68cbcbb92adf919f88c6c4359e0fb894174c
diego jake <student@appacademy.io>
2014-06-23 11:26:11 -0400

    Send messages to the server on chat input form submit

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,4 +1,5 @@
 var socket = io.connect();
+var chat = new Chat(socket);
 
 var appendMessage = function(message) {
   $('.chatbox').append(message);
@@ -7,6 +8,14 @@ var appendMessage = function(message) {
 $(document).ready(function(){
   socket.on('serverMessage', function(data){
     appendMessage(data);
+  });
+
+  $('body').on('submit', 'form.chatinput', function(event) {
+    event.preventDefault();
+    
+    var message = $(this).find('input[name="message"]').val();
+    
+    chat.sendMessage(message);
   });
 })
 
